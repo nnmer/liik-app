3e7a7a0b94791a8cec489620fdbc2021d036f512
Anton Romanov <anton.v.romanov@gmail.com>
2023-10-15 20:47:37 +0800

    update deps for tauri: log and window position

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_log::{LogTarget};
+use tauri::{Manager};
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_positioner::init())
-        // This is required to get tray-relative positions to work
-        // .on_system_tray_event(|app, event| {
-        //     tauri_plugin_positioner::on_tray_event(app, &event);
-        // })
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+
+            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+        }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_log::Builder::default().targets([
+            LogTarget::LogDir,
+            LogTarget::Stdout,
+            LogTarget::Webview,
+        ]).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-import { move_window, Position } from "tauri-plugin-positioner-api";
-move_window(Position.TopRight);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
