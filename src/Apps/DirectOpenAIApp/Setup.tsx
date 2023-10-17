8a5f45fd30fdc3c6d10e01bd3206022b126cea74
Anton Romanov <anton.v.romanov@gmail.com>
2023-10-17 21:42:25 +0800

    send OpenAI key to config

--- src/Apps/DirectOpenAIApp/Setup.tsx
+++ src/Apps/DirectOpenAIApp/Setup.tsx
@@ -1,22 +1,34 @@
 import {Card, Text, Button, Center, Title, TextInput, Space} from '@mantine/core'
+import Config from '../../modules/Config'
+import { useRef } from 'react'
 
 export default function Setup() {
-    const submit = (value: string) => {
 
+    const accessKey = useRef()
+
+    const submit = async (value: string|null) => {
+        console.warn(value)
+        await Config.updateKey(Config.ref.apps.OpenAIDirect.accessKey, value)
+        
     }
-    
+
     return (
         <Center mih={'100vh'}> 
         <Card shadow="sm" padding="lg" radius="md" withBorder>
-            <Title order={3}>OpenAI access key</Title>
+            <Title order={3}>OpenAI models</Title>
             <Text>Enter your personal OpenAI access key. </Text>
             <Text>It will be used to communicate with OpenAI service and execute your requests.</Text>
             <Space h="xl" />
             <TextInput 
-                placeholder='access key' 
-                onChange={(event)=>submit(event.currentTarget.value)}
+                placeholder='OpenAI access key'      
+                ref={accessKey}          
             />
-            <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+            <Button variant="light" color="blue" fullWidth mt="md" radius="md"
+                onClick={(event)=>{
+                    event.preventDefault()
+                    return submit(accessKey.current.value)
+                }}
+            >
                 Save & Go
             </Button>
         </Card>
--- src/Apps/DirectOpenAIApp/index.tsx
+++ src/Apps/DirectOpenAIApp/index.tsx
@@ -1,9 +1,22 @@
+import Config from "../../modules/Config";
 import Setup from "./Setup";
+
+function App () {
+    return (
+        <>OPenAI </>
+    )
+}
 
 export default function DirectOpenAI() {
     return (
         <div>
-            <Setup></Setup>
+            {
+                Config.get().apps.OpenAIDirect.enabled
+                && Config.get().apps.OpenAIDirect.accessKey?.length > 0
+                ? <App/>
+                : <Setup />
+            }
+            
         </div>
     )
 }
