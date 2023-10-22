b8233a3303a82c082a7c293dc1819add2f1bee40
Anton Romanov <anton.v.romanov@gmail.com>
2023-10-22 22:33:09 +0800

    add sentry sdk

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,25 @@ import { MantineProvider } from '@mantine/core';
 import theme from "./theme.ts";
 
 import { Config } from "./modules/Config";
+import * as Sentry from '@sentry/react'
+
+if (import.meta.env.PROD) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        // tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 Config.init()
 .then(()=>{
